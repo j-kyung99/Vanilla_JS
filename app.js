e509f0ecb2ff59945bbcef2cebb07e1224fb4199
j-kyung99 <86589951+j-kyung99@users.noreply.github.com>
2022-11-12 15:04:47 +0900

    #2.10

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const player = {
-  name: "nico",
-  sayHello: function (otherPersonName) {
-    console.log("hello " + otherPersonName + " nice to meet you!");
+const calculator = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  divide: function (a, b) {
+    console.log(a / b);
+  },
+  multiple: function (a, b) {
+    console.log(a * b);
+  },
+  power: function (a, b) {
+    console.log(a ** b);
   },
 };
 
-console.log(player.name);
-player.sayHello(player.name);
+calculator.add(9, 3);
+calculator.minus(9, 3);
+calculator.divide(9, 3);
+calculator.multiple(9, 3);
+calculator.power(9, 3);
