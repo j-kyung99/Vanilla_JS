960e1f0e7cfd664bd45efa71d0afe22949783962
j-kyung99 <86589951+j-kyung99@users.noreply.github.com>
2022-12-02 14:56:23 +0900

    modify - 3

--- js/weather.js
+++ js/weather.js
@@ -10,7 +10,9 @@ function onGeoOK(position) {
       const weather = document.querySelector("#weather span:first-child");
       const city = document.querySelector("#weather span:last-child");
       city.innerText = data.name;
-      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      let temp = data.main.temp;
+      temp = Math.round(temp * 10) / 10;
+      weather.innerText = `${data.weather[0].main} / ${temp}°C`;
     });
 }
 
